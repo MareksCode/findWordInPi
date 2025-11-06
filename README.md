e24f4331e7f604bb797496ff138fba6c1e024d9f
Marek <119450312+MareksCode@users.noreply.github.com>
2025-11-06 16:52:06 +0000

    add

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # findWordInPi
 ever wanted to search a word in the digits of pi? Me neither
+
+pi.txt contains 1 million digits of pi. For bigger results you have to input a bigger "pi source"
---
+++ library.c
@@ -0,0 +1,67 @@
+#include "library.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node {
+    char data;
+    struct Node* next;
+    struct Node* prev;
+} Node;
+typedef struct NumNode {
+    double data;
+    struct NumNode* next;
+    struct NumNode* prev;
+} NumNode;
+
+char getCharFromTwoNumbers(int numbers) {
+    return (char)('a' + numbers % 26);
+}
+
+void searchPiFor(char *word) {
+    FILE *filePointer = fopen("./pi.txt", "r");
+    if (filePointer == NULL) {
+        printf("Could not open file\n");
+        return;
+    }
+    char ch;
+    char prevCh = 0;
+    int counter = 0;
+    char* fullStringNum = malloc(sizeof(char) * 2);
+    int currentReconstructIndex = 0;
+    long int returnCounter = 0;
+    while ((ch = fgetc(filePointer)) != EOF) {
+        counter = (counter + 1)%2;
+        returnCounter += 1;
+        if (counter == 1) {
+            prevCh = ch;
+        } else {
+            fullStringNum[0] = ch;
+            fullStringNum[1] = prevCh;
+
+            char parsedCharacter = (char)((int)'a'+(atoi(fullStringNum)%26));
+           // printf("parsedCharacter: %c\n", parsedCharacter);
+
+            if (word[currentReconstructIndex] == parsedCharacter) {
+                currentReconstructIndex += 1;
+            } else {
+                currentReconstructIndex = 0;
+            }
+
+            if (currentReconstructIndex == strlen(word)) {
+                printf("Found the word! @%llu", returnCounter-strlen(word));
+                free(fullStringNum);
+                return;
+            }
+        }
+    }
+    printf("Found nothing");
+    free(fullStringNum);
+}
+
+int main() {
+    char word[] = "hey";
+    searchPiFor(word);
+    return 0;
+}
